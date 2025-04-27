6e4e2974ea66dbd7a195f6c437c2af4323ee0071
luo0715 <a0975140151j@gmail>
2025-04-27 17:59:14 +0800

    home-page

--- app/page.js
+++ app/page.js
@@ -1,46 +1,31 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import styles from "./page.module.css";
 import HeroSection from "./components/HeroSection";
 import StartBtn from "./components/button/StartBtn";
 import About from "./components/About";
-import Link from "next/link";
-import PlanIcon from "./components/icon/PlanIcon";
-import QuestionBankIcon from "./components/icon/QuestionBankIcon-";
-import StudyGuidesIcon from "./components/icon/StudyGuidesIcon";
+import Menu from "./components/Menu";
+import Animation from "./components/Animation";
 
 export default function page() {
   const [isActive, setIsActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(function () {
+    const fadeOutTimer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(fadeOutTimer);
+  }, []);
 
   return (
     <div className={styles.container}>
+      {isLoading ? <Animation /> : ""}
       <NavBar isActive={isActive} onIsActive={setIsActive} />
-      {isActive ? (
-        <>
-          <div className={styles["menu-box"]}>
-            <h1 className={styles.h1}>Students</h1>
-            <div className={styles.option}>
-              <Link href="" className={styles.link}>
-                <PlanIcon />
-                <button className={styles.btn}>Question Bank</button>
-              </Link>
-              <Link href="" className={styles.link}>
-                <QuestionBankIcon />
-                <button className={styles.btn}>My Plan</button>
-              </Link>
-              <Link href="" className={styles.link}>
-                <StudyGuidesIcon />
-                <button className={styles.btn}>Study Guides</button>
-              </Link>
-            </div>
-          </div>
-          <div className={styles.overlay} onClick={() => setIsActive((isActive) => !isActive)}></div>
-        </>
-      ) : (
-        ""
-      )}
+      {isActive ? <Menu onIsActive={setIsActive} /> : ""}
       <div className={styles.content}>
         <HeroSection />
         <StartBtn>Start answering</StartBtn>
